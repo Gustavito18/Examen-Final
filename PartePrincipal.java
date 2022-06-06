bb9e07b121de6526e7e545d8c5195f2f6722b49f
Gustavito18 <lmayorgao@miumg.edu.gt>
2022-06-05 21:08:52 -0600

    Parte cuatro

--- PartePrincipal.java
+++ PartePrincipal.java
@@ -100,62 +100,73 @@ public class PartePrincipal {
 		                       System.out.println("3. Regresar");
 		                       
 		                     System.out.println("Ingrese la opcion que desea usar: ");
-
-	              }
+		                     
 		                     decisionTres =sc.nextInt();
 		                     
 		                	 
 		   	              switch(decisionTres){
+		                  case 1:
+			                    
+		                      String nombreD="";
+		                      int numeroM=0;
+		                      String nombreC="";
+		                      int registroA=0;
+		 
+		                        departamentosB.departamento();
+		                          
 
-		   	                case 1:
-		   	                    
-		   	                      String nombreD="";
-		   	                      int numeroM=0;
-		   	                      String nombreC="";
-		   	                      int registroA=0;
-		   	 
-		   	                        departamentosB.departamento();
-		   	                          
+			          break;
+		                  
+		                  case 2:
+		                      
+		                      departamentosB.departamentoNumero();
+		                     
+			          break;
+		                  
 
-		   		          break;
-		   	                  
-		   	                  case 2:
-		   	                      
-		   	                      departamentosB.departamentoNumero();
-		   	                     
-		   		          break;
-		   	                  
+		                case 3:
+		                     
+			          break;
+		                }
+		              
+		             }while(decisionTres!=3);
+		   
+		             break;
+		                     
+		                 
+		                case 3:
+		                  
+		                           ejerciciosListasB.mostrarDatos();
 
-		   	                case 3:
-		   	                     
-		   		          break;
-		   	                }
-		   	              
-		   	             }while(decisionTres!=3);
-		   	   
-		   	             break;
-		   	                     
-		   	                 
-		   	                case 3:
-		   	                  
-		   	                           ejerciciosListasB.mostrarDatos();
+		                 break;
+		                 
+		                case 4:
+		                        
+		                           departamentosB.listaHistorial();
+		                     
+		                 break;
+		                 
+		                  case 5:
+		                        
+		                           departamentosB.limpieza();
+		                     
+		                 break;
+		                
+		                 
+		                case 6:
+		                              
+		                 break;
+		                 
+			              }
+			         
+			         }while(decision!=6);        
+			    
+			         System.out.println("Se ha cerrado el programa");
+			    }
+			
+			}
 
-		   	                 break;
-		   	                 
-		   	                case 4:
-		   	                        
-		   	                           departamentosB.listaHistorial();
-		   	                     
-		   	                 break;
-		   	                 
-		   	                  case 5:
-		   	                        
-		   	                           departamentosB.limpieza();
-		   	                     
-		   	                 break;
-		   	                
-		   	                 
-		   	                case 6:
-		   	                              
 
+	          
+		                
 
